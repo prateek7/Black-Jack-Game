08ec6cc1829736141b711c2c990138b36a4a8bc5
prateek7 <sahrawa@uwindsor.ca>
2017-09-03 20:55:45 -0400

    Duplicate

--- E-commerce/admin/language/english/common/footer.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Text
-$_['text_footer'] 	= '<a href="http://sahrawa.myweb.cs.uwindsor.ca">Prats BookStore</a> &copy; 20014-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] 	= 'Version %s';
